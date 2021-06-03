dc2ba95009d21f73103d918722268886855f85c5
LOCPNH\huulo <huuloc.phamnguyen@gmail.com>
2021-06-03 21:58:56 +0700

    Commit Model

---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Abtracts
+{
+    public interface IAuditable
+    {
+        DateTime CreatedDate { get; set; }
+        string CreatedBy { get; set; }
+        DateTime? UpdatedDate { get; set; }
+        string UpdatedBy { get; set; }
+        string MetaKeyword { set; get; }
+        string MetaDescription { set; get; }
+        bool Status { set; get; }
+    }
+}
